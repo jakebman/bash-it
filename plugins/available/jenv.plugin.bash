0da747bb8772a655717daf909e4963d06560d99e
Jake Boeckerman <C-jake.boeckerman@charter.com>
2017-12-27 17:12:51 -0700

    Save 1s by not rehashing jenv on each new shell

--- plugins/available/jenv.plugin.bash
+++ plugins/available/jenv.plugin.bash
@@ -4,4 +4,4 @@ about-plugin 'load jenv, if you are using it'
 export JENV_ROOT="$HOME/.jenv"
 pathmunge "$JENV_ROOT/bin"
 
-if which jenv > /dev/null; then eval "$(jenv init - bash)"; fi
+if which jenv > /dev/null; then eval "$(jenv init - bash --no-rehash)"; fi
