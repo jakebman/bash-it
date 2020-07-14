8666df53061c9496d7626265dcab72b02c374bae
Jake Boeckerman <jakebman@gmail.com>
2020-07-14 16:19:58 -0600

    Add wsl command itself to WSL plugin

--- plugins/available/wsl.plugin.bash
+++ plugins/available/wsl.plugin.bash
@@ -3,14 +3,14 @@ about-plugin 'Windows Subsystem for Linux interop'
 
 __init_wsl() {
   local NPP='/mnt/c/Program Files (x86)/Notepad++'
-
-  if [ -d "$NPP" ] ; then 
+  if [ -d "$NPP" ] ; then
     PATH="$PATH:$NPP"
     alias npp='notepad++.exe'
     alias notepad++='notepad++.exe'
     alias notepad='notepad++.exe'
   fi
   alias explorer='explorer.exe'
+  alias wsl='wsl.exe'
 
   local DOCK='/mnt/c/Program Files/Docker/Docker/resources/bin'
   if [ -d "$DOCK" ] ; then
