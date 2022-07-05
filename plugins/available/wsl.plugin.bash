26e5e2e75477c147d5bd0ad0c048cb35e245c576
Jake Boeckerman <jake.boeckerman@charter.com>
2022-07-05 15:32:47 -0600

    Use minimal git for WSL 1 repos
    (and use proper checking for maven)

--- plugins/available/wsl.plugin.bash
+++ plugins/available/wsl.plugin.bash
@@ -19,9 +19,15 @@ __init_wsl() {
     alias docker=docker.exe
   fi
 
-  if which mvn &>/dev/null ; then
+  if _command_exists mvn ; then
     export MAVEN_OPTS="-Dmaven.repo.local='${WINDOWS_HOME}/.m2/repository'"
   fi
+
+  # wsl.exe prints in window-y output (utf-16, CRLF), so we need to undo that for our unix tools
+  local BASH_IT_WSL_VERSION=$(wsl.exe --list --running --verbose | iconv --from-code UTF-16LE --to-code UTF-8 | dos2unix | grep  --word-regex "${WSL_DISTRO_NAME}" | awk '{print $NF}')
+
+  # git is slow on version 1 WSLs. We save a bunch by doing this minimally
+  [ "$BASH_IT_WSL_VERSION" == 1 ] && export SCM_GIT_SHOW_MINIMAL_INFO=true
 }
 
 __init_wsl
