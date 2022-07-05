dec1bb013efcc3a160e96216a79af6796543ed3e
Jake Boeckerman <jake.boeckerman@charter.com>
2022-07-05 15:55:35 -0600

    more typos

--- aliases/available/jake.aliases.bash
+++ aliases/available/jake.aliases.bash
@@ -4,6 +4,9 @@ about-alias "Jake's custom aliases for common typos"
 alias vmi=vim
 alias gti=git
 alias igt=git
+alias vit=git
 alias :q="echo You are not in vim"
 
 alias lsa='ls -a'
+
+alias grpe=gre
