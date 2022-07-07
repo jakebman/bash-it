12638f8158372b6fdb8336bd4143536215b21c06
Jake Boeckerman <jake.boeckerman@charter.com>
2022-07-06 19:21:45 -0600

    useful functions

--- custom/jake.bash
+++ custom/jake.bash
@@ -2,3 +2,13 @@
 export EDITOR=vim
 export VISUAL=vim
 
+function vars {
+	# magic incantation from the internet
+	# Basically, prints the variables and functions of 
+	# the current bash session, but doesn't print the functions
+	(set -o posix; set)
+}
+
+function jake_debug {
+	echo `date +"%r"` "$@" >>~/jake-bashit-debug
+}
