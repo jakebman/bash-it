312072cdefde92b5b59c69870316d92638939c59
Jake Boeckerman <jake.boeckerman@charter.com>
2022-07-07 10:27:45 -0600

    Better behavior for LESS

--- custom/jake.bash
+++ custom/jake.bash
@@ -2,6 +2,12 @@
 export EDITOR=vim
 export VISUAL=vim
 
+# Allow less to simply dump the output to STDOUT when it would all fit on a single page
+# https://stackoverflow.com/questions/2183900/how-do-i-prevent-git-diff-from-using-a-pager
+# no-init disables that weird 'second screen' behavior, which I don't like
+# RAW... enables color interpretation
+export LESS="--quit-if-one-screen --no-init --RAW-CONTROL-CHARS"
+
 function vars {
 	# magic incantation from the internet
 	# Basically, prints the variables and functions of 
