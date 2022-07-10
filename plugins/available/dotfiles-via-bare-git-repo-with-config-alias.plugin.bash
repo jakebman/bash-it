d0376d3a517e06041a68746846886c928434c526
Jake Boeckerman <jakebman@gmail.com>
2022-07-09 21:23:18 -0600

    one way of tracking dotfiles

---
+++ plugins/available/dotfiles-via-bare-git-repo-with-config-alias.plugin.bash
@@ -0,0 +1,17 @@
+
+# One of the options described in https://news.ycombinator.com/item?id=11070797
+# at https://news.ycombinator.com/item?id=11070797
+about "Following the ideas at https://www.atlassian.com/git/tutorials/dotfiles"
+
+: "${BASH_IT_DOTFILES_GIT_REPO:=~/.cfg}"
+if ! [ -d "${BASH_IT_DOTFILES_GIT_REPO}" ] ; then
+  _log_debug "${BASH_IT_DOTFILES_GIT_REPO} is not a valid git dir. Try 'git init --bare ${BASH_IT_DOTFILES_GIT_REPO}' to create it"
+  return
+fi
+_log_debug ""
+alias config='/usr/bin/git --git-dir="${BASH_IT_DOTFILES_GIT_REPO}" --work-tree="$HOME"'
+config config --local status.showUntrackedFiles no
+
+
+
+
