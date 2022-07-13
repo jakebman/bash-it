72c0955a2b93e4f4e8b15debc7ada434a5984c65
Jake Boeckerman <jake.boeckerman@charter.com>
2022-07-12 23:20:56 -0600

    tree isn't actually a typo, but gitt is

--- aliases/available/jake.aliases.bash
+++ aliases/available/jake.aliases.bash
@@ -1,6 +1,9 @@
 # shellcheck shell=bash
 about-alias "Jake's custom aliases for common typos"
 
+# Not actually a typo
+alias tree='tree -C' # color tree
+
 alias vmi=vim
 alias gim=vim
 alias bim=vim
@@ -9,13 +12,14 @@ alias gti=git
 alias igt=git
 alias vit=git
 alias bit=git
+alias gitt=git
+
 alias :q="echo You are not in vim"
 alias q="echo no need to quit - you are already out"
 
 alias lsa='ls -a'
 
 alias les=less
-alias tree='tree -C' # color tree
 alias ree=tree
 
 alias vd=cd
