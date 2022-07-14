6fa862494b979a1eab3d92766300846dbe4f39be
Jake Boeckerman <jakebman@gmail.com>
2022-07-13 19:30:22 -0600

    use variables for man and bin folders

--- custom/jake.bash
+++ custom/jake.bash
@@ -36,11 +36,14 @@ function vars {
 alias var=vars # because I'm lazy
 
 function update-ack-and-its-manpages {
+  local bin_dir="${HOME}/bin"
+  local man_dir="${bin_dir}/man"
   # I'm learning about manpages, so this first implementation is likely bad
-  mkdir -p ~/bin/man/man1
+  mkdir -p "${man_dir}/man1" # ack's manpage goes in man1. Add others here as needed
+
   # instructions from https://beyondgrep.com/install/
   # I wish they had a -latest option.
-  if ! curl https://beyondgrep.com/ack-v3.5.0 > ~/bin/ack && chmod 0755 ~/bin/ack ; then
+  if ! curl https://beyondgrep.com/ack-v3.5.0 > "${bin_dir}/ack" && chmod 0755 "${bin_dir}/ack" ; then
     echo "failed"
     return 1
   fi
@@ -48,24 +51,24 @@ function update-ack-and-its-manpages {
   echo # spacing
 
   # set up the manpath within ~/bin; I don't want to have to maintain ~/man
-  if grep "$HOME/bin/man" "${HOME}/.manpath" &>/dev/null ; then
-    echo "your user-specific manpath config already knows about ~/bin/man. Woohoo!"
+  if grep "$man_dir" "${HOME}/.manpath" &>/dev/null ; then
+    echo "your user-specific manpath config already knows about ${man_dir}. Woohoo!"
   else
-    echo "adding a section to your ~/.manpath file"
+    echo "adding a section to your ~/.manpath file that looks like this:"
     cat <<END | tee --append "${HOME}/.manpath"
 # this section was added automatically by my ackrc-creation script. I really hope it didn't break anything
 # -Jake Boeckerman
-MANDATORY_MANPATH ${HOME}/bin/man
-MANPATH_MAP ${HOME}/bin ${HOME}/bin/man
+MANDATORY_MANPATH ${man_dir}
+MANPATH_MAP ${bin_dir} ${man_dir}
 END
   fi
 
   echo # spacing
 
   if _command_exists pod2man ; then
-    local manfile=~/bin/man/man1/ack.1p
+    local manfile="${man_dir}/man1/ack.1p"
     pod2man ~/bin/ack >$manfile
-    echo "manfile created at ~/bin/man/man2/ack.1p. It looks like:"
+    echo "manfile created at ${manfile}. It looks like:"
     ls -l "$manfile"
   else
     echo "please install pod2man. Probably via apt install perl"
@@ -75,9 +78,9 @@ END
   echo # spacing
 
   if _command_exists mandb ; then
-    mandb --user-db
+    mandb --user-db || return 1
     echo # spacing
-    echo mandb updated :D
+    echo mandb user db updated
   else
     echo "please install mandb. Otherwise, this won't work"
     return 1
