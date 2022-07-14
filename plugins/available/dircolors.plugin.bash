878af5e95cd06c8f04f8540df0886a2eff7f9705
Jake Boeckerman <jakebman@gmail.com>
2022-07-14 12:25:26 -0600

    comment the source of this script

--- plugins/available/dircolors.plugin.bash
+++ plugins/available/dircolors.plugin.bash
@@ -1,5 +1,6 @@
 
 about-plugin "enable color support of ls and also add handy aliases"
+# Shamelessly stolen from Ubuntu's default .bashrc
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
