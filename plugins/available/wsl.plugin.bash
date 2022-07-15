bbf12dfced12fe9a0f1f5869717edcfd98a7a9b3
Jake Boeckerman <jakebman@gmail.com>
2022-07-14 18:17:10 -0600

    Change the posture of check_existing to diagnostics
    We change _check... to _has..., which changes its pass/fail-ness
    And we moved the log level checking to before the _command_exists call

--- plugins/available/wsl.plugin.bash
+++ plugins/available/wsl.plugin.bash
@@ -119,28 +119,25 @@ _wsl-find-a-windows-exe() {
   return 1
 }
 
-_check_exiting_commands () {
-  about "diagnostics about existing aliases or programs"
+_has_existing_commands () {
+  about "Whether \$1 has any aliases or names that aren't \$2"
   param "1: the bare name you expect to exist"
   param "2: the expected value of an existing alias (if the bare name already references this alias, no warning is emitted)"
   group 'wsl'
 
   local bare_name="$1"
   if _command_exists_silently "$bare_name" ; then
-    if [[ "${BASH_IT_LOG_LEVEL:-0}" -lt "${BASH_IT_LOG_LEVEL_TRACE?}" ]] ; then
-      return 1 # no need to check most of the time
-    fi
-
-    # this logging is noisy:
-    # _log_debug "log level is ${BASH_IT_LOG_LEVEL:-0}, finding previous commands of $bare_name"
     local exe_file="$2"
     if type "$bare_name" | grep "${bare_name} is aliased to \`'${exe_file}''" &>/dev/null ; then
-      # _log_debug "${exe_file} is already aliased by ${bare_name} (nothing to do)"
-      return 1
+      _log_debug "${exe_file} is already aliased by ${bare_name} (nothing to do)"
+      return
     fi
     _log_warning "An existing command supercedes ${bare_name}: $(type "$bare_name")"
-    return 1
+    return
   fi
+
+  _log_debug "No existing command found for ${bare_name}"
+  return 1
 }
 
 _wsl-alias-a-windows-exe() {
@@ -165,10 +162,10 @@ _wsl-alias-a-windows-exe() {
 
   local bare_name="$(basename "$WIN_EXE" | sed -e 's/\.exe$//g')" # strips .exe suffix
 
-  #if ! _check_exiting_commands "$bare_name" "$WIN_EXE" ; then
-  #  unset WIN_EXE # clear the env from the return code of _wsl-find-a-windows-exe
-  #  return 1
-  # fi
+  if [ "${BASH_IT_LOG_LEVEL:-0}" -ge "${BASH_IT_LOG_LEVEL_INFO?}" ] &&  _has_existing_commands "$bare_name" "$WIN_EXE" ; then
+    unset WIN_EXE # clear the env from the return code of _wsl-find-a-windows-exe
+    return 1
+   fi
 
   alias "${bare_name}='${WIN_EXE}'" || _log_error "could not create alias '${bare_name}' for '${WIN_EXE}'"
 
