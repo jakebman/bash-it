3699fac67ae3161852403b2e1e6886e395015c8f
Jake Boeckerman <jake.boeckerman@charter.com>
2022-07-18 17:19:28 -0600

    undo misguided alias for -C on tree; *actually* use CLICOLOR_FORCE

--- plugins/available/dircolors.plugin.bash
+++ plugins/available/dircolors.plugin.bash
@@ -10,5 +10,4 @@ if [ -x /usr/bin/dircolors ]; then
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
-    _command_exists tree && alias tree='tree -C'
 fi
