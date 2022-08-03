f0a74d7c28a8904bcb36a15ef3a7089bb2cdc5a6
Jake Boeckerman <jake.boeckerman@charter.com>
2022-08-02 23:19:45 -0600

    allow lls and ltree to take arguments

--- aliases/available/jake-aliases.aliases.bash
+++ aliases/available/jake-aliases.aliases.bash
@@ -3,11 +3,18 @@ about-alias "Jake's custom commands that are aliases"
 
 alias tulpn='netstat -tulpn'
 
-# custom commands (they have typo entries, too)
-alias lls='ll --color | less'
-alias ltree='tree | less'
+# They're functionally aliases; so sue me
+# (They also have typo aliases)
+function lls {
+  ll --color "$@" | less
+}
 
-# It's functionally an alias; so sue me
+function ltree {
+  # I already set CLICOLOR_FORCE, so -C is not required, but it's more consistent to set it here
+  # minor rant: why doesn't tree have a long option for this?
+  tree -C "$@" | less
+}
+
 function jqless {
   jq --color-output "$@" | less
 }
