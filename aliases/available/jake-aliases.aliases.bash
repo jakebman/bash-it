8f3e6ec25864f6e1ac195f46ba9d3e9828572d61
Jake Boeckerman <jakebman@gmail.com>
2022-08-08 16:12:16 -0600

    shellcheck wants tab indents

--- aliases/available/jake-aliases.aliases.bash
+++ aliases/available/jake-aliases.aliases.bash
@@ -6,15 +6,15 @@ alias tulpn='netstat -tulpn'
 # They're functionally aliases; so sue me
 # (They also have typo aliases)
 function lls {
-  ll --color "$@" | less
+	ll --color "$@" | less
 }
 
 function ltree {
-  # I already set CLICOLOR_FORCE, so -C is not required, but it's more consistent to set it here
-  # minor rant: why doesn't tree have a long option for this?
-  tree -C "$@" | less
+	# I already set CLICOLOR_FORCE, so -C is not required, but it's more consistent to set it here
+	# minor rant: why doesn't tree have a long option for this?
+	tree -C "$@" | less
 }
 
 function jqless {
-  jq --color-output "$@" | less
+	jq --color-output "$@" | less
 }
