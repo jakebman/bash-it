a8e33de17ee3e752b0b983c7dbe3cdc4c96c8b0c
Jake Boeckerman <jake.boeckerman@charter.com>
2022-11-08 12:11:38 -0700

    optional: customizability for _jq-ified scripts

--- plugins/available/jq.plugin.bash
+++ plugins/available/jq.plugin.bash
@@ -3,11 +3,13 @@
 function _jq-ify {
   # TODO: try and actually respect the parameter orders of our parameters
   # and figure out which is actually supposed to be the argument files
+  about "use \$JQ_FILTER to choose a smaller section of the files to compare!"
   local cmd="$1"
   local left="$2"
   local right="$3"
+  local filter="${JQ_FILTER:-.}"
   shift 3
-  "$cmd" "$@" <(jq -S . <"$left") <(jq -S . <"$right")
+  "$cmd" "$@" <(jq -S "$filter" <"$left") <(jq -S "$filter" <"$right")
 }
 
 alias jqdiff="_jq-ify diff"
