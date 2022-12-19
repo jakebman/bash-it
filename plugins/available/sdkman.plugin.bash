0dccc6ea9ad306c296a7d885e652e142688c9f7f
Jake Boeckerman <jake.boeckerman@charter.com>
2022-12-19 16:23:43 -0700

    verbose install instructions for sdkman plugin

--- plugins/available/sdkman.plugin.bash
+++ plugins/available/sdkman.plugin.bash
@@ -5,4 +5,8 @@ about-plugin 'Load Software Development Kit Manager'
 # otherwise default to ~/.sdkman
 export SDKMAN_DIR=${SDKMAN_DIR:-$HOME/.sdkman}
 
-[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+if [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]]; then
+	source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+else
+	_log_error "please install SDKMAN - 'curl https://get.sdkman.io | bash' or read more at https://sdkman.io"
+fi
