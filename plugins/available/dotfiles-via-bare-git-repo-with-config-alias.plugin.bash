11a20410daba97dda7f3705ad300ac1d8e15fcd6
Jake Boeckerman <jakebman@gmail.com>
2023-03-25 18:24:53 -0600

    Embed my learnings from getting git highlighting into bat/j
    Basically, libgit2 doesn't respect GIT_WORK_TREE, which works in concert
    with GIT_DIR to choose which work tree you're using. However, you can
    instead set core.worktree to get the same behavior, and libgit2 does
    respect this situation

--- plugins/available/dotfiles-via-bare-git-repo-with-config-alias.plugin.bash
+++ plugins/available/dotfiles-via-bare-git-repo-with-config-alias.plugin.bash
@@ -11,13 +11,24 @@ if ! [ -d "${BASH_IT_DOTFILES_GIT_REPO}" ] ; then
   _log_error "Try 'git init --bare \${BASH_IT_DOTFILES_GIT_REPO}' to create it,"
   _log_error "Or 'git clone \${YOUR_GIT_URL} --bare \${BASH_IT_DOTFILES_GIT_REPO}' to get it from somewhere else"
   _log_error "or set BASH_IT_DOTFILES_GIT_REPO to your bare git directory"
+  _log_error 'And once you have that, remove the core.bare setting and replace it with core.worktree=$HOME'
+  _log_error "(This lets my (modified) bat work with my custom j jumping script)"
+  _log_error 'GIT_DIR=$BASH_IT_DOTFILES_GIT_REPO (git config --unset core.bare; git config --add core.worktree="$HOME")'
   return
 fi
 
 _log_debug "found dotfiles repository at ${BASH_IT_DOTFILES_GIT_REPO}"
-alias config='GIT_DIR="${BASH_IT_DOTFILES_GIT_REPO}" GIT_WORK_TREE="$HOME" git'
+alias config='GIT_DIR="${BASH_IT_DOTFILES_GIT_REPO}" git'
 
 if [ "no" != "$(config config status.showUntrackedFiles)" ] ; then
   _log_error "config dotfiles repo is not configured to hide untracked files - this is very likely to be a huge performance burden"
   _log_error "Try 'config config --local status.showUntrackedFiles no' to set this"
+fi
+
+if ! config config core.worktree &>/dev/null ; then
+  # libgit2 doesn't support GIT_WORK_TREE, but it does support core.worktree. I'd prefer to use
+  # GIT_WORK_TREE, but since bat uses libgit2's rust bindings, *it* doesn't support GIT_WORK_TREE.
+  # And therefore I ultimately don't get git integration unless we do it this way
+  _log_error "Please set core.worktree to the proper location (probably \$HOME)"
+  _log_error 'config config --unset core.bare; config config --add core.worktree="$HOME"'
 fi
