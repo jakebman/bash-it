6db41db3b8268eeeb0989a17c950a9fd674760b6
Jake Boeckerman <jakebman@gmail.com>
2023-05-19 14:24:45 -0600

    vimwhich tool

--- custom/jake.bash
+++ custom/jake.bash
@@ -83,6 +83,11 @@ function hgrep {
   history | grep "$@"
 }
 
+function vimwhich {
+	vim "$(which "$1")"
+}
+alias vimw=vimwhich
+
 function diff {
   about "allow you to type the bare word 'diff' and get an automatic git diff, while still not harming the diff command"
   if [[ "$#" -eq 0 ]] ; then
