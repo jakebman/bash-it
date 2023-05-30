ac57ec01a70abbb11e507fbec459e9a3ed781415
Jake Boeckerman <jakebman@gmail.com>
2023-05-30 12:26:14 -0600

    no need to check for editorconfig - it's a submodule of the dotfiles directory

--- plugins/available/jake-install-tools.plugin.bash
+++ plugins/available/jake-install-tools.plugin.bash
@@ -186,19 +186,6 @@ function jake-install-tools() {
     esac
   fi
 
-  local VIM_EDITORCONFIG_DIR="${HOME}/.vim/pack/editorconfig/start"
-  if ! [ -d "$VIM_EDITORCONFIG_DIR" ] ; then
-    echo "Vim would like having editorconfig support - install it with:"
-    echo -en "\t"
-    echo "# mkdir -p $VIM_EDITORCONFIG_DIR; cd $VIM_EDITORCONFIG_DIR; git clone https://github.com/editorconfig/editorconfig-vim.git"
-    echo -en "\t"
-    echo "# or use submodules:"
-    echo -en "\t"
-    echo "config submodule update --init --remote"
-  else
-    echo "Nothing to do for vim editorconfig - vim's editorconfig is happy at ~/.vim/pack/editorconfig"
-  fi
-
   # don't need these, but should report them anyway
   _jake-check-optional-tools
 
