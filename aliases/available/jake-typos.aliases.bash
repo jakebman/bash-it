6a03b0ecc8868f7f43618427a8cdb3c552d79750
Jake Boeckerman <jakebman@gmail.com>
2023-08-26 19:13:42 -0600

    typo

--- aliases/available/jake-typos.aliases.bash
+++ aliases/available/jake-typos.aliases.bash
@@ -145,6 +145,7 @@ alias comm9t=commit
 alias comm0t=commit
 alias comm-t=commit
 alias commt=commit
+alias cimmit=commit
 alias commit-a='commit -a'
 alias commita='commit -a'
 alias ignroed=ignored
