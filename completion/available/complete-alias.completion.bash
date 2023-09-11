992d89f75d4339e9f4ca0558a79197850078a447
Jake Boeckerman <jakebman@gmail.com>
2023-09-10 23:25:51 -0600

    a complete-alias plugin

---
+++ completion/available/complete-alias.completion.bash
@@ -0,0 +1,16 @@
+# shellcheck shell=bash
+about-plugin "Use cykerway's complete-alias project to complete aliases"
+# Load after all aliases and completions to understand what needs to be completed
+# BASH_IT_LOAD_PRIORITY: 800
+
+# From https://github.com/cykerway/complete-alias
+
+: "${COMPLETE_ALIAS_FILE:=${HOME}/.complete-alias/complete_alias}"
+
+if [ -f "${COMPLETE_ALIAS_FILE}" ]; then
+	source "${COMPLETE_ALIAS_FILE}"
+else
+	_log_error "please install complete-alias from https://github.com/cykerway/complete-alias, or point \$COMPLETE_ALIAS_FILE to the complete_alias file within the place you checked it out from"
+fi
+
+complete -F _complete_alias "${!BASH_ALIASES[@]}"
