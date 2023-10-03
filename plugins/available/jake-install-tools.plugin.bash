e1756b72e7e092fb7c51243ddb80bb4df379be3e
Jake Boeckerman <jakebman@gmail.com>
2023-10-03 16:18:58 -0600

    ability to look up what depends on an apt package

--- plugins/available/jake-install-tools.plugin.bash
+++ plugins/available/jake-install-tools.plugin.bash
@@ -90,6 +90,7 @@ function jake-install-tools() {
   _jake-find-tool xmlindent # doesn't have --long-options, which is a little weird, but formats all XML (incl. xmllint --xpath results), so that's good
   _jake-find-tool dos2unix
   _jake-find-tool neofetch
+  _jake-find-tool debtree
   _jake-find-tool thefuck
   _jake-find-tool unzip
   _jake-find-tool clang
