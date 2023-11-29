fe4137fd94dc429547fb98f4abafa066ddf646df
Jake Boeckerman <jakebman@gmail.com>
2023-11-29 10:17:18 -0700

    musing about mktemp

--- plugins/available/curl-logging.plugin.bash
+++ plugins/available/curl-logging.plugin.bash
@@ -6,7 +6,7 @@ about-plugin 'keep some facts around about what happens with curl output'
 _command_exists curl || return # don't create the function if the binary is missing
 
 function _curl-logging-helper {
-	# TODO: log rotation/storage concerns
+	# TODO: log rotation/storage concerns. Potentially using mktemp?
 	(
 		# NB: I prefer %q over shell-quote for shell quoting. This note lives here to remind myself
 		# TODO: I'm honestly a little curious the difference between `shell-quote` and printf "%q" ...
