bef9f590e297367b6526389ebb81e8a8b5336eac
Jake Boeckerman <jakebman@gmail.com>
2023-12-12 16:54:57 -0700

    I installed cmake at home

--- plugins/available/jake-install-tools.plugin.bash
+++ plugins/available/jake-install-tools.plugin.bash
@@ -95,6 +95,7 @@ function jake-install-tools() {
   _jake-find-tool unzip
   _jake-find-tool clang
   _jake-find-tool iotop
+  _jake-find-tool cmake cmake 'optional - used by the reMarkable suite'
   # _jake-find-tool g++ # I prefer clang for now
   _jake-find-tool tree
   _jake-find-tool zip
