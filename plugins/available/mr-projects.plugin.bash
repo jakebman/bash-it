9cb0220a24ce9a5d3dfc9c6ec92aa86c1c887c46
Jake Boeckerman <jakebman@gmail.com>
2023-12-12 17:51:05 -0700

    wouldn't it be cool to get mr data into pj?

---
+++ plugins/available/mr-projects.plugin.bash
@@ -0,0 +1,58 @@
+# shellcheck shell=bash
+about-plugin 'quickly navigate projects that mr is tracking (either in ~/.mrconfig or a chained one'
+
+# TODO: basically everything
+: "${BASH_IT_PROJECT_PATHS:=$HOME/Projects:$HOME/src:$HOME/work}"
+
+function pj() {
+	about 'navigate quickly to your various project directories'
+	group 'projects'
+
+	local proj="${1?${FUNCNAME[0]}: project name required}"
+	local cmd PS3 dest d
+	local -a dests
+
+	if [[ "$proj" == "open" ]]; then
+		shift
+		proj="${1}"
+		cmd="${EDITOR?}"
+	fi
+
+	# collect possible destinations to account for directories
+	# with the same name in project directories
+	IFS=':' read -ra dests <<< "${BASH_IT_PROJECT_PATHS?${FUNCNAME[0]}: project working folders must be configured}"
+	for d in "${!dests[@]}"; do
+		if [[ ! -d "${dests[d]}/${proj}" ]]; then
+			unset 'dests[d]'
+		fi
+	done
+
+	case ${#dests[@]} in
+		0)
+			_log_error "BASH_IT_PROJECT_PATHS must contain at least one existing location"
+			return 1
+			;;
+		1)
+			dest="${dests[*]}/${proj}"
+			;;
+		*) # TODO: what if we print the others, but go to the FIRST match
+			PS3="Multiple project directories found. Please select one: "
+			dests+=("cancel")
+			select d in "${dests[@]}"; do
+				case $d in
+					"cancel")
+						return
+						;;
+					*)
+						dest="${d}/${proj}"
+						break
+						;;
+				esac
+			done
+			;;
+	esac
+
+	"${cmd:-cd}" "${dest}"
+}
+
+alias pjo="pj open"
