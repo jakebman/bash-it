51876f3603a8270efd285775780a18cbabc8f973
Jake Boeckerman <jakebman@gmail.com>
2024-01-22 16:42:52 -0700

    unify all the cdd[ddd] commands to use a single helper method

--- plugins/available/jake-cdd.plugin.bash
+++ plugins/available/jake-cdd.plugin.bash
@@ -1,42 +1,45 @@
 # shellcheck shell=bash
 about-plugin 'some functions (cdd - go to parent folder of; cddd - go to grandparent folder of)'
 
+# TODO: it might make an interesting exercise for these commands to *also* search in $CDPATH
+
+function _cdd_dirname () {
+	about "dirname doesn't know about . and .. entries. This method does"
+
+	case "$(basename "$1")" in
+		. | .. )
+			echo "${1}/.."
+			;;
+		*)
+			dirname "$dir"
+			;;
+	esac
+}
+
+function _cdd_any () {
+	param "1: any number of additional '../..' suffixes to add to the first implicit .. (see the definition of cdd and cddd to see base cases for this)"
+	param "2: cd-like argument, optional. Defaults to '.' (pwd)"
+
+	local arg="${2:-.}"
+	cd "$(_cdd_dirname "${arg}")/${1}"
+}
+
 function cdd () {
-  if [ "$#" -eq 0 ] ; then
-    cd ..
-  elif [ -d "$1" ] ; then
-    cd "$1/.." # because cd `dirname .` doesn't go up
-  else
-    cd "$(dirname "$1")"
-  fi
+	_cdd_any "" "$@"
 }
 
 function cddd () {
-  if [ "$#" -eq 0 ] ; then
-    cd ../..
-  elif [ -d "$1" ] ; then
-    cd "$1/../.."
-  else
-    cd "$(dirname "$1")/.."
-  fi
+	_cdd_any ".." "$@"
 }
 
 function cdddd () {
-  if [ "$#" -eq 0 ] ; then
-    cd ../../..
-  elif [ -d "$1" ] ; then
-    cd "$1/../../.."
-  else
-    cd "$(dirname "$1")/../.."
-  fi
+	_cdd_any "../.." "$@"
 }
 
 function cddddd () {
-  if [ "$#" -eq 0 ] ; then
-    cd ../../../..
-  elif [ -d "$1" ] ; then
-    cd "$1/../../../.."
-  else
-    cd "$(dirname "$1")/../../.."
-  fi
+	_cdd_any "../../.." "$@"
+}
+
+function cdddddd () {
+	_cdd_any "../../../.." "$@"
 }
