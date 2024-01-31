115c674fa24dd871e7763f6502b264becf340336
Jake Boeckerman <jakebman@gmail.com>
2024-01-31 13:28:28 -0700

    shfmt - batcat.aliases

--- aliases/available/batcat.aliases.bash
+++ aliases/available/batcat.aliases.bash
@@ -1,5 +1,5 @@
 # shellcheck shell=bash
 about-alias 'walk back the batcat name for bat ("a cat(1) clone with wings")'
-if _command_exists batcat ; then
+if _command_exists batcat; then
 	alias bat=batcat
 fi
