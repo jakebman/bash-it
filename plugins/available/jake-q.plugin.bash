d266c0471ce4cad4135b3e88e44b64566569805c
Jake Boeckerman <jakebman@gmail.com>
2024-02-01 14:32:21 -0700

    better (more-canonical) way to determine whether we're in a login shell

--- plugins/available/jake-q.plugin.bash
+++ plugins/available/jake-q.plugin.bash
@@ -4,9 +4,12 @@ about-plugin "q - an exit that doesn't exit your login shell (this overrides the
 
 unalias q
 function q {
-	if grep ^- /proc/$$/cmdline -q; then
-		# NB: this can be faked by running bash --login, for instance
-		echo "You're at a top-level (--login) shell. Exiting here will end the terminal session"
+	if shopt -q login_shell; then
+		# NB: this isn't *necessarily* the top-most bash. You can manually invoke a login shell
+		# wherever you like by invoking `bash --login`
+		echo "You're at a top-level or login shell. Exiting here will end the terminal session"
+		# or in terse phrasing like `logout` uses:
+		# echo "$0: is login shell. Use \`logout' or \`exit'"
 	else
 		echo "You're not at the top-level login. Exiting to your parent"
 		exit
