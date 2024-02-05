ba87b01b083f48b27b3c1e5454ad7afb1270057b
Jake Boeckerman <jakebman@gmail.com>
2024-02-05 16:08:07 -0700

    shfmt

--- plugins/available/jq.plugin.bash
+++ plugins/available/jq.plugin.bash
@@ -1,31 +1,30 @@
 #! /bin/bash
 
 function _jq-ify {
-  # TODO: try and actually respect the parameter orders of our parameters
-  # and figure out which is actually supposed to be the argument files
-  about "use \$JQ_FILTER to choose a smaller section of the files to compare!"
-  local cmd="$1"
-  local left="$2"
-  local right="$3"
-  local filter="${JQ_FILTER:-.}"
-  shift 3
-  "$cmd" "$@" <(jq -S "$filter" <"$left") <(jq -S "$filter" <"$right")
+	# TODO: try and actually respect the parameter orders of our parameters
+	# and figure out which is actually supposed to be the argument files
+	about "use \$JQ_FILTER to choose a smaller section of the files to compare!"
+	local cmd="$1"
+	local left="$2"
+	local right="$3"
+	local filter="${JQ_FILTER:-.}"
+	shift 3
+	"$cmd" "$@" <(jq -S "$filter" < "$left") <(jq -S "$filter" < "$right")
 }
 
 alias jqdiff="_jq-ify diff"
 alias jqvimdiff="_jq-ify vimdiff"
 alias vimjqdiff="_jq-ify vimdiff"
 
-
 function jqless {
 	local args
 
-	if [ -t 1 ] ; then
+	if [ -t 1 ]; then
 		# terminal output - color it
 		args+=(--color-output)
 	fi
 
-	if [[ $# -eq 0 ]] || [[ -f "$1" ]] ; then
+	if [[ $# -eq 0 ]] || [[ -f "$1" ]]; then
 		# If the user doesn't specify a filter as the first argument,
 		# which means:
 		# * no arguments (presume STDIN) or
