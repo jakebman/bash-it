3b2f7c1ea9dac34ad15b703045699bd010561ca9
Jake Boeckerman <jakebman@gmail.com>
2024-02-17 19:49:02 -0700

    git updated

--- plugins/available/jake-install-tools.plugin.bash
+++ plugins/available/jake-install-tools.plugin.bash
@@ -158,7 +158,7 @@ function jake-install-tools() {
 
 	# I would really prefer to use the latest git
 	local GIT_VERSION="$(git --version)"
-	local EXPECTED_VERSION='git version 2.43.0'
+	local EXPECTED_VERSION='git version 2.43.2'
 	if [ "$GIT_VERSION_MAJOR" != "$EXPECTED_VERSION" ]; then
 		local GIT_VERSION_MAJOR=$(echo $GIT_VERSION | sed -E -n 's/.* ([0-9]+)\..*/\1/p')
 		local GIT_VERSION_MINOR=$(echo $GIT_VERSION | sed -E -n 's/.* ([0-9]+)\.([0-9]+)\..*/\2/p')
