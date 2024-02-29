fce6464c1dd6d69a4c57b47585d1bab5241a2f5a
Jake Boeckerman <jakebman@gmail.com>
2024-02-29 11:31:04 -0700

    fix the test here - should look for existence of any file, not just a regular file

--- plugins/available/jake-cd-implicit-foo-folder.plugin.bash
+++ plugins/available/jake-cd-implicit-foo-folder.plugin.bash
@@ -2,7 +2,7 @@
 about-plugin 'auto-create a folder named foo if you cd into it'
 
 function cd {
-	if [[ "$#" -eq 1 ]] && [[ "foo" = "$1" ]] && ! [[ -f "foo" ]]; then
+	if [[ "$#" -eq 1 ]] && [[ "foo" = "$1" ]] && ! [[ -e "foo" ]]; then
 		mkdir foo
 	fi
 	builtin cd "$@"
