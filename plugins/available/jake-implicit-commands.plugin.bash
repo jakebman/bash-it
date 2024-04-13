5d58709f8f7932ffd75dfcca1537e8b272a9c1e6
Jake Boeckerman <jakebman@gmail.com>
2024-04-12 22:51:51 -0600

    Duplicate jake-implicit-commands into jake-words (cleanup)
    Delete the unused portions of the duplicated files

--- plugins/available/jake-implicit-commands.plugin.bash
+++ plugins/available/jake-implicit-commands.plugin.bash
@@ -176,39 +176,6 @@ function _is_flag {
 	return 0
 }
 
-function words {
-	about 'print the word list, paged. Arguments are ack filters on a word list (flags apply to the next non-flag argument, which is assumed to be a regex). If stdin is not a terminal, it is used in lieu of the ${WORDLIST:/usr/share/dict/words}'
-	# Implementation note: this is *incredibly recursive*
-
-	if [[ -t 0 ]]; then
-		cat "${WORDLIST:-/usr/share/dict/words}" | words "$@"
-		return # Not strictly necessary if EVERYTHING is if/elif/else. But that can't be guaranteed locally
-	elif [[ "$#" -eq 0 ]]; then
-		less
-		return # see above
-	else
-		# Grab a bunch of flags, and apply them to "the current" regex
-		local -a args
-		while _is_flag "$1"; do
-			args+=("$1")
-			shift
-		done
-
-		args+=("$1")
-
-		# error checking - there should be at least one argument left, so shift should succeed
-		if ! shift; then
-			echo "not enough arguments after flags '${args[@]}'" >&2
-			return 1
-		fi
-
-		ack "${args[@]}" | words "$@"
-		return # see above
-	fi
-
-	# PUTTING CODE HERE MIGHT BE A VERY BAD IDEA. SEE ABOVE
-}
-
 function pulls {
 	about "try to mange pull requests from the CLI"
 	if remotes | grep --quiet gitlab; then
