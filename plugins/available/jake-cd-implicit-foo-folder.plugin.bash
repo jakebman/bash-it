03e99bc45bc20012a8990f72ffe5f0cef775fdfe
Jake Boeckerman <jakebman@gmail.com>
2024-05-22 10:58:53 -0600

    change the meaning of `cd foo` to "just go" to ~/foo instead

--- plugins/available/jake-cd-implicit-foo-folder.plugin.bash
+++ plugins/available/jake-cd-implicit-foo-folder.plugin.bash
@@ -1,9 +1,16 @@
 # shellcheck shell=bash
-about-plugin 'auto-create a folder named foo if you cd into it'
+about-plugin 'Pretend ~/foo is a valid target to cd into, from anywhere (as if its parent dir is in CDPATH)'
+
+# TODO: this behavior could become generic ("$BASH_IT_CDPATH_ITEMS"). When it does, it is almost like `pj`, so maybe an integration becomes in-order
 
 function cd {
 	if [[ "$#" -eq 1 ]] && [[ "foo" = "$1" ]] && ! [[ -e "foo" ]]; then
-		mkdir foo
+		# emulate the following CDPATH behavior:
+		# cd:
+		# If a non-empty directory name from CDPATH is used ... and the directory change is successful,
+		# the absolute pathname of the new working directory is written to the standard output.
+		builtin cd ~/foo && echo ~/foo
+	else
+		builtin cd "$@"
 	fi
-	builtin cd "$@"
 }
