73283c048c354adf20d0a601fb3a06a9c8651443
Jake Boeckerman <jakebman@gmail.com>
2024-05-24 14:45:16 -0600

    commit myself to allow un-quoted spaces in commit messages

--- aliases/available/jake-aliases.aliases.bash
+++ aliases/available/jake-aliases.aliases.bash
@@ -157,6 +157,7 @@ function cherry-pick {
 }
 
 # commit with one argument is either add/commit the file, or commit with the given message
+# TODO: in a situation where no flags are specified, -m is "$*", and we automatically addp any files mentioned
 function commit {
 	# stash some flags that can be "transparent" to this feature
 	# (these can only be BEFORE the message for now... potentially always)
