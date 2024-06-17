0ce8e4d2d1e439fbf4288f7c4002d46f0386dbbe
Jake Boeckerman <jakebman@gmail.com>
2024-06-17 10:04:30 -0600

    typo

--- plugins/available/google.plugin.bash
+++ plugins/available/google.plugin.bash
@@ -3,7 +3,7 @@ cite about-plugin
 about-plugin 'perform google searches from the cli with the google command'
 
 function google() {
-	about 'parform a google search'
+	about 'perform a google search'
 	param '*: a series of words that are concatenated into the google query. TODO: you currently need to urlencode ampersands yourself'
 	# TODO: urlencode ampersands in the query
 	# TODO: but maybe parameters that start with & and have an = (ex: `&lang=en`) are unmolested. Prepend a space (like `google " &lang=en"`) to prevent this.
