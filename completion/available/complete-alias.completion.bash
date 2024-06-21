6965f219e27f6c61966783cd1aaedbfc58b151a5
Jake Boeckerman <jakebman@gmail.com>
2024-06-21 16:19:15 -0600

    realize that the coordination between typos and autocomplete doesn't need to be as strong as I thought

--- completion/available/complete-alias.completion.bash
+++ completion/available/complete-alias.completion.bash
@@ -6,7 +6,7 @@ about-plugin "Use cykerway's complete-alias project to complete aliases"
 # From https://github.com/cykerway/complete-alias
 # TODO: It'd be nice if this were bundled into the /vendor folder
 
-# TODO: it would be nice if this didn't also complete my typos (This will definitely requre coordination)
+# TODO: it would be nice if this didn't also complete my typos (This will definitely requre coordination... unless I just declare the typo aliases... after this code runs (`BASH_IT_LOAD_PRIORITY: 801`))
 
 function _use_complete_alias {
 	# Configuration. User can choose a folder, filename, or the whole path. Whole path wins, and its value is written back over any that it superceded
