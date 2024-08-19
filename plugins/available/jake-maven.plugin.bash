808f58eb30c2f6eafb2a31c0b6cc69f1ab873023
Jake Boeckerman <jakebman@gmail.com>
2024-08-19 17:31:22 -0600

    automatic maven paging!

--- plugins/available/jake-maven.plugin.bash
+++ plugins/available/jake-maven.plugin.bash
@@ -1,3 +1,16 @@
 about-plugin "jake lazy maven commands"
 
 alias go-offline='mvn dependency:go-offline'
+
+# Implicit paging!
+function mvn {
+	# output to terminal
+	if [ -t 1 ]; then
+		echo "Jake: paging maven output :D"
+
+		# TODO: where is style.color documented?
+		command mvn "$@" -Dstyle.color=always | less --RAW-CONTROL-CHARS
+	else
+		command mvn "$@"
+	fi
+}
