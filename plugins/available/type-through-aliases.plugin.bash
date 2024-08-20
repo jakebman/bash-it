c9c85d94630f7a43a9029473205dc105f9846370
Jake Boeckerman <jakebman@gmail.com>
2024-08-20 10:53:19 -0600

    draft of type-through-aliases

---
+++ plugins/available/type-through-aliases.plugin.bash
@@ -0,0 +1,19 @@
+
+
+function type {
+	# TODO: `about` docs, recursive drilling down until no longer getting an alias
+	case `command type -t -- "$1"` in
+		alias)
+			# https://askubuntu.com/a/871435/235107
+			local next #="${BASH_ALIASES[$1]}"
+			command type "$@"
+			# https://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
+			read next args <<< "${BASH_ALIASES[$1]}"
+			command type "$next"
+			;;
+		*)
+			command type "$@"
+			;;
+	esac
+}
+
