cb153c75c6166b8f5f5986202ffe67d6ac2ba115
Jake Boeckerman <jakebman@gmail.com>
2024-08-20 14:05:33 -0600

    make it impossible to do the previously-doc'd VERY BAD IDEA by separating the base case and fixup case from the rest of the code

--- plugins/available/jake-words.plugin.bash
+++ plugins/available/jake-words.plugin.bash
@@ -4,36 +4,38 @@ function words {
 	about 'print the word list, paged. Arguments are ack filters on a word list (flags apply to the next non-flag argument, which is assumed to be a regex). If stdin is not a terminal, it is used in lieu of the ${WORDLIST:/usr/share/dict/words}'
 	# Implementation note: this is *incredibly recursive*
 
+	# Base cases
 	if [[ -t 0 ]]; then
+		# First invocation: take wordlist implicitly if no other stdin is specified
 		cat "${WORDLIST:-/usr/share/dict/words}" | words "$@"
-		return # Not strictly necessary if EVERYTHING is if/elif/else. But that can't be guaranteed locally
+		return
 	elif [[ "$#" -eq 0 ]]; then
+		# Last invocation: all the filters from all of "$@" have applied.
+		# We unconditionally invoke the pager and assume it'll not page unless necessary
 		pager
-		return # see above
-	else
-		# Grab a bunch of flags, and apply them to "the current" regex
-		local -a args
-		while _is_flag "$1"; do
-			args+=("$1")
-			shift
-		done
-
-		# TODO: I'd like to keep words -c . a e i o u to count the final result, not return no results because 123,456 doesn't contain a e i o or u
-		args+=("$1")
-
-		# error checking - there should be at least one argument left, so shift should succeed
-		if ! shift; then
-			echo "not enough arguments after flags '${args[@]}'" >&2
-			return 1
-		fi
-
-		(
-			set -o pipefail
-			ack "${args[@]}" | words "$@"
-		)
+		return
 	fi
 
-	# PUTTING CODE HERE MIGHT BE A VERY BAD IDEA. SEE ABOVE
+	# Grab a bunch of flags, and apply them to "the current" regex
+	local -a args
+	while _is_flag "$1"; do
+		args+=("$1")
+		shift
+	done
+
+	# TODO: I'd like to keep words -c . a e i o u to count the final result, not return no results because 123,456 doesn't contain a e i o or u
+	args+=("$1")
+
+	# error checking - there should be at least one argument left, so shift should succeed
+	if ! shift; then
+		echo "not enough arguments after flags '${args[@]}'" >&2
+		return 1
+	fi
+
+	(
+		set -o pipefail
+		ack "${args[@]}" | words "$@"
+	)
 }
 
 function wordle {
