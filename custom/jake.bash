96cabacb8bd879197e830eddcbb4f9e828f46975
Jake Boeckerman <jakebman@gmail.com>
2024-08-22 13:48:35 -0600

    choose my AWS BROWSER

--- custom/jake.bash
+++ custom/jake.bash
@@ -3,6 +3,8 @@ export EDITOR=vim
 export VISUAL=vim
 export PAGER=less
 export BASHIT_CURL_PAGER='bat --style=numbers'
+# NB: an enhancement only provided by aws-auto-login
+export AWS_BROWSER=wslview
 
 # Allow j!! to work for a previous ack query
 alias jack=j
