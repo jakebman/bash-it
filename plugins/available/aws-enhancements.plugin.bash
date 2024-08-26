286a9874c32c7af6bc44bb5d3a88c7138ddd3580
Jake Boeckerman <jakebman@gmail.com>
2024-08-26 11:32:46 -0600

    allow exceptions for aws commands that *should not* autologin

--- plugins/available/aws-enhancements.plugin.bash
+++ plugins/available/aws-enhancements.plugin.bash
@@ -2,10 +2,28 @@
 cite about-plugin
 about-plugin "AWS automatically login if there's a failure, allow a custom browser setting via AWS_BROWSER"
 
+_BASH_IT_AWS_AUTOLOGIN_EXCEPTIONS=(sso sts)
+# It's worth adding your own exceptions for login/logout aws-cli aliases that you create
+# _BASH_IT_AWS_AUTOLOGIN_EXCEPTIONS+=(login loggedin logout)
+
+function _aws-autologin-exception {
+	about "given an aws cli command, should the aws-autologin behavior ignore it?"
+	# https://stackoverflow.com/questions/3685970/check-if-a-bash-array-contains-a-value
+	local exception;
+	for exception in "${_BASH_IT_AWS_AUTOLOGIN_EXCEPTIONS[@]}"; do
+		if [ "$exception" = "$1" ]; then
+			return
+		fi
+	done
+	return 1
+}
+
 function aws-autologin {
 	about "automatically log in to aws sso if you're not"
-	# NB: it's useful to create an alias in aws cli for this command. Imagine 'aws loggedin' here
-	if ! command aws sts get-caller-identity &>/dev/null; then
+	if _aws-autologin-exception "$@"; then
+		_log_debug "not automatically logging into aws to run '$@' - it is an exception"
+	elif ! command aws sts get-caller-identity &>/dev/null; then
+		# NB: it's useful to create an alias in aws cli for this sts command. Imagine 'aws loggedin' here
 		echo "You're not logged in to aws sso. Automatically logging you in!"
 		command aws sso login || return
 		echo
