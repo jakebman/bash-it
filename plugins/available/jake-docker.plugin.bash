ef4e535ae9ebee1439e25875437625e1f51c691a
Jake Boeckerman <jakebman@gmail.com>
2024-08-28 20:33:49 -0600

    shfmt

--- plugins/available/jake-docker.plugin.bash
+++ plugins/available/jake-docker.plugin.bash
@@ -3,7 +3,7 @@ about-plugin 'Helpers to more easily work with Docker'
 
 : ${BASH_IT_DOCKER_ADHOC_TAG:=adhocRunTarget}
 function docker-build-and-run {
-  about "build the current docker, and immediately run it with -it and --rm"
-  group 'docker'
-  docker build --tag "${BASH_IT_DOCKER_ADHOC_TAG}" "$@" && docker run --rm -it "${BASH_IT_DOCKER_ADHOC_TAG}"
+	about "build the current docker, and immediately run it with -it and --rm"
+	group 'docker'
+	docker build --tag "${BASH_IT_DOCKER_ADHOC_TAG}" "$@" && docker run --rm -it "${BASH_IT_DOCKER_ADHOC_TAG}"
 }
