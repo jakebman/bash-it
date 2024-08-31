a5fe4bd226fab41645a1b4c9357bcb5d83383bd2
Jake Boeckerman <jakebman@gmail.com>
2024-08-30 21:38:38 -0600

    my own counting function, finally

---
+++ plugins/available/jake-counting-lines.plugin.bash
@@ -0,0 +1,7 @@
+# shellcheck shell=bash
+about-plugin 'get that sort | uniq -c | sort -n magic into a single command'
+
+function counting {
+	about 'sort the input lines by their frequency. Args are passed as-if to `sort -n `'
+	sort | uniq -c | sort -n "$@" | pager
+}
