3264f0daa1e6b0abbea68763d70c08f747d3cf5e
Jake Boeckerman <jakebman@gmail.com>
2024-09-03 12:17:57 -0600

    refactor out docker-run so I can use it, too

--- plugins/available/jake-docker.plugin.bash
+++ plugins/available/jake-docker.plugin.bash
@@ -1,9 +1,17 @@
 cite about-plugin
 about-plugin 'Helpers to more easily work with Docker'
 
+
+function docker-run {
+	about 'transiently run a docker image (essentially -it, --rm)'
+	group 'docker'
+	docker run -it --rm "$@"
+}
+
+
 : ${BASH_IT_DOCKER_ADHOC_TAG:=adhocRunTarget}
 function docker-build-and-run {
 	about "build the current docker, and immediately run it with -it and --rm"
 	group 'docker'
-	docker build --tag "${BASH_IT_DOCKER_ADHOC_TAG}" "$@" && docker run --rm -it "${BASH_IT_DOCKER_ADHOC_TAG}"
+	docker build --tag "${BASH_IT_DOCKER_ADHOC_TAG}" "$@" && docker-run "${BASH_IT_DOCKER_ADHOC_TAG}"
 }
