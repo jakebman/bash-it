05cf030514f65cbf555378347c6b268060c75c60
Jake Boeckerman <jakebman@gmail.com>
2024-09-04 10:49:17 -0600

    if there are no folders, use the local dir to build in

--- plugins/available/jake-docker.plugin.bash
+++ plugins/available/jake-docker.plugin.bash
@@ -14,8 +14,16 @@ function docker-build-and-run {
 	about "build the current docker, and immediately run it with -it and --rm"
 	group 'docker'
 	local -a args
-	if [[ $# -eq 0 ]]; then
+	local arg haveBuildDir
+	for arg; do
+		# Over-broad. It'll grab --output-dir some-dir/
+		if [ -d "$arg" ]; then
+			haveBuildDir=true
+		fi
+	done
+	if [ true != "$haveBuildDir" ]; then
 		args+=(.)
 	fi
-	docker build --tag "${BASH_IT_DOCKER_ADHOC_TAG}" "${args[@]}" "$@" && docker-run "${BASH_IT_DOCKER_ADHOC_TAG}"
+
+	docker build --tag "${BASH_IT_DOCKER_ADHOC_TAG}" "$@" "${args[@]}" && docker-run "${BASH_IT_DOCKER_ADHOC_TAG}"
 }
