c89f9c6554f0aa722f00cb35c6149b7510f4069f
Jake Boeckerman <jakebman@gmail.com>
2024-09-17 11:33:52 -0600

    recognize the implementation of this function might miss recursive function renaming

--- plugins/available/jake-typos.plugin.bash
+++ plugins/available/jake-typos.plugin.bash
@@ -45,6 +45,7 @@ function _typo-builtin {
 
 
 # https://mharrison.org/post/bashfunctionoverride/
+# NB: this won't cover recursive functions properly
 function save_function {
     local ORIG_FUNC=$(declare -f $1)
     local NEWNAME_FUNC="$2${ORIG_FUNC#$1}"
