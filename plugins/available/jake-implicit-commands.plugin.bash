61827cfa480e3b7cb83d5e21f26e1b0ceeb5c45d
Jake Boeckerman <jakebman@gmail.com>
2024-09-17 16:49:35 -0600

    bypass the typo mechanism here, and use the appropriate git alias

--- plugins/available/jake-implicit-commands.plugin.bash
+++ plugins/available/jake-implicit-commands.plugin.bash
@@ -239,7 +239,7 @@ function du {
 function browse {
 	about "allow you to type the bare word 'browse' and get an automatic gh browse, while not stepping on the toes of xdg-utils's browse command (a symlink to xdg-open), which takes arguments"
 	if [[ "$#" -eq 0 ]]; then
-		if remotes | grep --quiet gitlab; then
+		if git remotes | grep --quiet gitlab; then
 			glab repo view -w "$@"
 		else
 			gh browse "$@"
