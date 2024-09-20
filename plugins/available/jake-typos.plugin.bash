258dbf49cc495e6ec77af1c73cea3c7b6893d78d
Jake Boeckerman <jakebman@gmail.com>
2024-09-20 17:08:02 -0600

    whoops - wrong name

--- plugins/available/jake-typos.plugin.bash
+++ plugins/available/jake-typos.plugin.bash
@@ -67,7 +67,7 @@ function command_not_found_handle {
 	# TODO: can I get a printed bash stack trace?
 	_log_debug "generated alias '$(type "$name")' for '$name'"
 	if type -af "${_BASH_IT_TYPOS["$name"]}" |& grep -s -q 'is a shell builtin$' &> /dev/null; then
-		_log_warn "$name is aliased to a builtin, ${_BASH_IT_TYPOS["$name"]}. It might be worth using the typo_builtin function instead"
+		_log_warning "$name is aliased to a builtin, ${_BASH_IT_TYPOS["$name"]}. It might be worth using the typo_builtin function instead"
 	fi
 
 	local - # local set -o stuff
