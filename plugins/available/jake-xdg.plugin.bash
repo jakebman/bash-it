b97a5029b155dee8e11438c6628b68205ac6ecdf
Jake Boeckerman <jakebman@gmail.com>
2024-12-10 13:07:54 -0700

    proper indentation

--- plugins/available/jake-xdg.plugin.bash
+++ plugins/available/jake-xdg.plugin.bash
@@ -84,7 +84,7 @@ export COMPLETE_ALIAS_DIR="${JAKE_XDG_BIN_DIR}/complete-alias"
 #     See find_global_config_path/get_global_path/USER_CONFIG_FILENAMES in https://github.com/GitGuardian/ggshield/blob/6d0d8b86c504e0066de5758c69d8cd95a4f09426/ggshield/core/constants.py#L19
 # .gitignore.d - vcsh, created by default. hardcoded name below $VCSH_BASE, which defaults to $HOME
 # .gitmodules - required because I'm keeping subrepos in my conf vcsh repo
-#    It needs to live in $GIT_WORK_TREE - gets really interesting with nns-config also having these
+#     It needs to live in $GIT_WORK_TREE - gets really interesting with nns-config also having these
 # .ivy2 - sbt (scala build tool)'s equivalent to .m2
 #     Potentialy configurable like java above, via:
 #         https://www.scala-sbt.org/1.x/docs/Library-Management.html#Ivy+Home+Directory
@@ -92,7 +92,7 @@ export COMPLETE_ALIAS_DIR="${JAKE_XDG_BIN_DIR}/complete-alias"
 #         https://www.scala-sbt.org/1.x/docs/Launcher-Configuration.html
 # .landscape - ubuntu-ism
 # .m2 - a symlink. Would otherwise require CLI argument to move settings.xml, which is the only thing I keep there anyway :(
-#       TODO: maven 3.9.0+ accept CLI arguments via $MAVEN_ARGS. See https://maven.apache.org/configure.html
+#     TODO: maven 3.9.0+ accept CLI arguments via $MAVEN_ARGS. See https://maven.apache.org/configure.html
 # .motd_shown - probably not doable
 # .mrconfig (& .mr which symlinks to beat .mrtrust at tab completion) - cannot be moved without a code change in mr tool
 # .netrc - conventional file name, from telnet. Location configurable in curl, but not telnet.
