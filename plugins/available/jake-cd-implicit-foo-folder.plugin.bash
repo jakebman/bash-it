3b6a791c37275436df4534a34d99371fc385c62c
Jake Boeckerman <jakebman@gmail.com>
2025-01-21 17:39:11 -0700

    genericize the implicit `cd foo` behavior, with reasonable configurability

--- plugins/available/jake-cd-implicit-foo-folder.plugin.bash
+++ plugins/available/jake-cd-implicit-foo-folder.plugin.bash
@@ -1,15 +1,21 @@
 # shellcheck shell=bash
-about-plugin 'Pretend ~/foo is a valid target to cd into, from anywhere (as if its parent dir is in CDPATH)'
+about-plugin 'Allow certain folders to always remain valid `cd` targets, from anywhere (as if their parent dir is in CDPATH)'
 
-# TODO: this behavior could become generic ("$BASH_IT_CDPATH_ITEMS"). When it does, it is almost like `pj`, so maybe an integration becomes in-order
+# Implementation note: we accomplish this by combining two independent mechanisms:
+# 1. Creating a special quick-access folder, with symlinks to chosen always-valid cd targets. That folder goes on CDPATH
+# 2. Change cd so any single-word target that doesn't exist from `.` gets an implicit -P
+
+# "Append the quick-access folder to the cd path"
+# Automagically, however this also creates CDPATH with the initial "." entry, because
+# "A null directory name is the same as the current directory", per `help cd`
+CDPATH+=":${XDG_CONFIG_HOME:-${HOME}/.config}/cd/quick-access"
 
 function cd {
-	if [[ "$#" -eq 1 ]] && [[ "foo" = "$1" ]] && ! [[ -e "foo" ]]; then
-		# emulate the following CDPATH behavior:
-		# cd:
-		# If a non-empty directory name from CDPATH is used ... and the directory change is successful,
-		# the absolute pathname of the new working directory is written to the standard output.
-		builtin cd ~/foo && echo ~/foo
+	about 'normal `cd`, but non-$CWD entries get an implicit -P to follow symlinks'
+	if [[ "$#" -eq 1 ]] && ! [[ -d "$1" ]]; then
+		# Shim: I'm basically assuming all symlinks contained in non-$CWD entries in CDPATH are "less canonical"
+		# than their targets.
+		builtin cd -P "$@"
 	else
 		builtin cd "$@"
 	fi
