13d6e46fee714fbf9e8fab61d2850364dff2326a
Jake Boeckerman <jakebman@gmail.com>
2025-02-11 15:27:53 -0700

    better word choice

--- plugins/available/jake-implicit-commands.plugin.bash
+++ plugins/available/jake-implicit-commands.plugin.bash
@@ -97,7 +97,7 @@ function diff {
 function dd {
 	about "sometimes I type dd when I meant d, which means diff"
 	if [[ "$#" -eq 0 && -t 1 && -t 0 && -t 2 ]]; then
-		>&2 echo "${FUNCNAME[0]} - no args, plus std pipes are all terminals. ${FUNCNAME[0]} is not a cat. Running diff instead."
+		>&2 echo "${FUNCNAME[0]} - no args, plus stdio are all terminals. ${FUNCNAME[0]} is not a cat. Running diff instead."
 		>&2 echo
 		diff "$@"
 	else
