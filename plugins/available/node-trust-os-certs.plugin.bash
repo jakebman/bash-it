0620b530a3286443159ef6e9d862a9f503f5d637
Jake Boeckerman <jakebman@gmail.com>
2025-05-13 15:24:37 -0600

    allow Node tools to also read OS-level certificate stores

---
+++ plugins/available/node-trust-os-certs.plugin.bash
@@ -0,0 +1,28 @@
+# shellcheck shell=bash
+cite about-plugin
+about-plugin 'Configure Node.js to also trust the OS-level certificates (via NODE_EXTRA_CA_CERTS)'
+
+# This is needed for (at least) the postman CLI
+
+function _node_search_ca_certs {
+	local candidate
+
+	# List from https://go.dev/src/crypto/x509/root_linux.go, suggested by:
+	# https://serverfault.com/questions/62496/ssl-certificate-location-on-unix-linux
+	for candidate in \
+		/etc/ssl/certs/ca-certificates.crt \
+		/etc/pki/tls/certs/ca-bundle.crt \
+		/etc/ssl/ca-bundle.pem \
+		/etc/pki/tls/cacert.pem \
+		/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem \
+		/etc/ssl/cert.pem; do
+
+		if [ -f "$candidate" ]; then
+			# see https://nodejs.org/api/cli.html#cli_node_extra_ca_certs_file
+			export NODE_EXTRA_CA_CERTS=$candidate
+			return
+		fi
+	done
+}
+
+_node_search_ca_certs
