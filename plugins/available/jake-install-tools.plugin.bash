d619b0d20f3d8d46e19a9db4d065c7fb47db44c4
Jake Boeckerman <jakebman@gmail.com>
2025-06-25 00:15:53 -0600

    also grab btop - it's overwhelming but pretty

--- plugins/available/jake-install-tools.plugin.bash
+++ plugins/available/jake-install-tools.plugin.bash
@@ -127,6 +127,7 @@ function jake-install-tools() {
 	_jake-find-tool unzip
 	_jake-find-tool clang
 	_jake-find-tool  htop
+	_jake-find-tool  btop btop "htop, but overwhelming and pretty"
 	_jake-find-tool iotop
 	_jake-find-tool iftop
 	_jake-find-tool gh gh "github cli"
