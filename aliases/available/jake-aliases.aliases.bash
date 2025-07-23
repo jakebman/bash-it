960b906e18860229a984bb5e1e16d1d1e7632972
Jake Boeckerman <jakebman@gmail.com>
2025-07-23 10:43:21 -0600

    access to rainbow-branches without a git prefix

--- aliases/available/jake-aliases.aliases.bash
+++ aliases/available/jake-aliases.aliases.bash
@@ -477,6 +477,7 @@ alias logn='git logn' # log with numstats
 alias logp='git logp' # log with patch
 alias logs='git logs' # log with stats (+++-- indicators)
 alias ls-untracked='git ls-untracked'
+alias rainbow-branches='git rainbow-branches'
 alias rainbow-all='git rainbow-all'       # explicitly --all form of rainbow output
 alias rainbow-here='git rainbow-here'     # approx. git log --oneline --graph, specifically only the current history (no --all)
 alias shallow='git shallow'               # a shallow clone (non-alphabetical to be beside its twin)
