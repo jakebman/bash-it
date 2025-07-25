9984671878a4d969d680c70a5cd7526f102df0fa
Jake Boeckerman <jakebman@gmail.com>
2025-07-25 13:00:27 -0600

    Ability to maintain a maximum page width for manpages

---
+++ plugins/available/max-manwidth.plugin.bash
@@ -0,0 +1,17 @@
+
+about-plugin 'Keep $MANWIDTH no larger than $MAX_MANWIDTH - watching $COLUMNS and enabling MANWIDTH=MAX_MANWIDTH only when the screen is wider'
+
+: ${MAX_MANWIDTH:=100}
+
+function _maxManwidth_MaintainMaxManWidth {
+	if (( COLUMNS > MAX_MANWIDTH )); then
+		export MANWIDTH=$MAX_MANWIDTH
+	else
+		unset MANWIDTH
+	fi
+}
+
+# A terminal can start up with a too-large size, and never receive SIGWINCH
+_maxManwidth_MaintainMaxManWidth
+
+trap _maxManwidth_MaintainMaxManWidth SIGWINCH
