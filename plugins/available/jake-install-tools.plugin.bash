a4ae6834681be90ce3b203e6963a4fb77bf12efa
Jake Boeckerman <jakebman@gmail.com>
2025-08-18 17:02:10 -0600

    jake-update-expected-git-version

--- plugins/available/jake-install-tools.plugin.bash
+++ plugins/available/jake-install-tools.plugin.bash
@@ -197,7 +197,7 @@ function jake-install-tools() {
 
 	# I would really prefer to use the latest git
 	local GIT_VERSION="$(git --version)"
-	local EXPECTED_VERSION='git version 2.50.1'
+	local EXPECTED_VERSION='git version 2.51.0'
 	# TODO: it would be nice to be able to compare versions better
 	# 1/2: Does https://github.com/fsaintjacques/semver-tool work?
 	# TODO: These ppas get disabled on a distupgrade. Is there a hook I can get to re-enable them?
