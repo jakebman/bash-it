ed392953379577e8599d8f29aa05a0627ab512ed
Jake Boeckerman <jakebman@gmail.com>
2025-09-11 16:45:56 -0600

    The aws-enhancements completion is identical to aws-cli's, but loads after alias completion

--- completion/available/aws-ehancements.completion.bash
+++ completion/available/aws-ehancements.completion.bash
@@ -1,4 +1,6 @@
 # shellcheck shell=bash
+# Load after the the completion for other aliases, which loads at 800
+# BASH_IT_LOAD_PRIORITY: 810
 
 if _command_exists aws_completer; then
 	complete -C "$(command -v aws_completer)" aws
