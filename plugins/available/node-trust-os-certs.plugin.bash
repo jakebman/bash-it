e437152674f77f0f96bd71e8cc7c8e64cdfa0d21
Jake Boeckerman <jakebman@gmail.com>
2025-10-03 13:12:24 -0600

    maybe an easier way to get node to respect my CA

--- plugins/available/node-trust-os-certs.plugin.bash
+++ plugins/available/node-trust-os-certs.plugin.bash
@@ -4,6 +4,10 @@ about-plugin 'Configure Node.js to also trust the OS-level certificates (via NOD
 
 # This is needed for (at least) the postman CLI
 
+# TODO: does --use-system-ca work? If so, NODE_OPTIONS+=" --use-system-ca " would be nice
+# ... eve though this isn't in https://nodejs.org/api/cli.html#node_optionsoptions :(
+# https://nodejs.org/api/cli.html#--use-system-ca
+
 function _node_search_ca_certs {
 	local candidate
 
