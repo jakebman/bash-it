13bfcfbd52a98a306d1229d094c7d9c24020b920
Jake Boeckerman <jakebman@gmail.com>
2025-10-24 16:48:50 -0600

    this is handled by the batcat aliases

--- plugins/available/bat-cat-batextras.plugin.bash
+++ plugins/available/bat-cat-batextras.plugin.bash
@@ -3,10 +3,6 @@
 cite about-plugin
 about-plugin 'Tools to extend supporting bat, the cat replacement'
 
-if _command_exists batcat && ! _command_exists bat; then
-	alias bat=batcat
-fi
-
 if [[ "${BASH_IT_BAT_FOR_MAN:-1}" == 1 ]]; then
 	export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
