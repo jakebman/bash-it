8f7f19ee0e97da2ed94eb1795630218ec0d9c0a3
Jake Boeckerman <jakebman@gmail.com>
2026-05-27 13:10:02 -0600

    move bashrc-based PJ calculations to a dedicated plugin

---
+++ plugins/available/projects-candidates.plugin.bash
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+about-plugin 'set BASH_IT_PROJECT_PATHS from BASH_IT_PROJECT_PATHS_CANDIDATES for pj'
+# BASH_IT_LOAD_PRIORITY: 240
+
+: "${BASH_IT_PROJECT_PATHS:=$HOME/Projects:$HOME/src:$HOME/work}"
+if [ -v BASH_IT_PROJECT_PATHS ]; then
+	_log_warning "BASH_IT_PROJECT_PATHS is already set. Not analyzing BASH_IT_PROJECT_PATHS_CANDIDATES"
+	return
+fi
+for candidate in "${BASH_IT_PROJECT_PATHS_CANDIDATES[@]}"; do
+	if [[ -d "$candidate" ]]; then
+		# magic: use the "use this value if this variable is set syntax (`:+`)
+		# to add a preceeding colon to each entry after the first
+		BASH_IT_PROJECT_PATHS+="${BASH_IT_PROJECT_PATHS:+:}${candidate}"
+	fi
+done
+unset -v candidate candidates
