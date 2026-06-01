d995a61a9d4796c85d05069bc8956d8a3438f167
Jake Boeckerman <jakebman@gmail.com>
2026-06-01 15:44:00 -0600

    don't accidentally set the path if it's empty :(

--- plugins/available/projects-candidates.plugin.bash
+++ plugins/available/projects-candidates.plugin.bash
@@ -3,7 +3,6 @@ about-plugin 'set BASH_IT_PROJECT_PATHS from BASH_IT_PROJECT_PATHS_CANDIDATES fo
 # We need to load before the projects plugin, with default priority 250
 # BASH_IT_LOAD_PRIORITY: 240
 
-: "${BASH_IT_PROJECT_PATHS:=$HOME/Projects:$HOME/src:$HOME/work}"
 if [ -v BASH_IT_PROJECT_PATHS ]; then
 	_log_warning "BASH_IT_PROJECT_PATHS is already set. Not analyzing BASH_IT_PROJECT_PATHS_CANDIDATES"
 	return
