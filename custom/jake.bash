c1de50ecbe0256bc1542ba457ad907a23f687292
Jake Boeckerman <jakebman@gmail.com>
2026-09-22 10:12:04 -0600

    I did the _log_warn mistake again

--- custom/jake.bash
+++ custom/jake.bash
@@ -31,7 +31,7 @@ if [ -f ~/.kiro/mcp-secrets.env ]; then
 	if [[ "700 [jakebman] [directory] 755 [jakebman] [directory] 600 [jakebman] [regular file] " = "$(stat --printf '%a [%U] [%F] ' ~ ~/.kiro ~/.kiro/mcp-secrets.env)" ]]; then
 		unsafe_source_envfile ~/.kiro/mcp-secrets.env
 	else
-		_log_warn "~/.kiro/mcp-secrets.env exists, but has the wrong permission or ownership up its parents"
+		_log_warning "~/.kiro/mcp-secrets.env exists, but has the wrong permission or ownership up its parents"
 	fi
 fi
 
